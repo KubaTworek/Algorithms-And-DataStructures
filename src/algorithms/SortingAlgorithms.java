5760590c3c7ed5cf3354ac18a7c90ad5bf53430b
KubaTworek <kubatvorek@gmail.com>
2022-07-16 18:17:10 +0200

    countingSort added
    CountingSort method has been added

--- src/algorithms/SortingAlgorithms.java
+++ src/algorithms/SortingAlgorithms.java
@@ -1,14 +1,9 @@
 package algorithms;
 
-import java.util.Arrays;
-
 public class SortingAlgorithms {
-    public static void helloAlgorithm(){
-        System.out.println("Hello Algorithm");
-    }
 
     public static void bubbleSort(int[] arr){
-        for(int element : arr){
+        for(int ignored : arr){
             for(int i=0; i<arr.length-1; i++){
                 if(arr[i] > arr[i+1]){
                     swap(arr, i, i+1);
@@ -19,7 +14,7 @@ public class SortingAlgorithms {
 
     public static void selectionSort(int[] arr){
         int lastIndex = arr.length-1;
-        for(int element : arr){
+        for(int ignored : arr){
             int maxIndex = 0;
                 for(int i=0; i<lastIndex; i++){
                     if(arr[maxIndex] < arr[i+1]){
@@ -31,7 +26,50 @@ public class SortingAlgorithms {
         }
     }
 
-    private static void swap(int arr[], int a, int b){
+/*    public static void insertionSort(int[] arr){
+        for(int i=1; i<arr.length; i++){
+            for(int j=i-1; j>=0; j--){
+                if(arr[i] < arr[j]){
+                    swap(arr,i,j);
+                }
+            }
+        }
+    }*/
+
+    public static void shellSort(int[] arr){
+
+    }
+
+    public static void mergeSort(int[] arr){
+
+    }
+
+    public static void quickSort(int[] arr){
+
+    }
+
+    public static void countingSort(int[] arr){
+        int[] arrTemp = {0,0,0,0,0,0,0,0,0,0};
+        for (int k : arr) {
+            for (int j = 0; j < arrTemp.length; j++) {
+                if (k == j + 1) {
+                    arrTemp[j]++;
+                }
+            }
+        }
+        int index = 0;
+        for(int i=0; i<arrTemp.length; i++){
+            for(int j=0; j<arrTemp[i]; j++){
+                arr[index++] = i+1;
+            }
+        }
+    }
+
+    public static void radixSort(int[] arr){
+
+    }
+
+    private static void swap(int[] arr, int a, int b){
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
--- src/pl/pjatk/Main.java
+++ src/pl/pjatk/Main.java
@@ -9,12 +9,22 @@ public class Main {
     public static void main(String[] args) {
         int[] arr1 = {3,35,-23,1,46,-90};
         int[] arr2 = {3,35,-23,1,46,-90};
+        int[] arr3 = {3,35,-23,1,46,-90};
+        int[] arr4 = {3,35,-23,1,46,-90};
+        int[] arr5 = {3,35,-23,1,46,-90};
+        int[] arr6 = {3,35,-23,1,46,-90};
+        int[] arr7 = {3,5,1,1,8,2,10,2};
+        int[] arr8 = {2564,8136,3410,3156,9265,3850};
 
         SortingAlgorithms.bubbleSort(arr1);
         System.out.println(Arrays.toString(arr1));
 
         SortingAlgorithms.selectionSort(arr2);
         System.out.println(Arrays.toString(arr2));
+        
+        SortingAlgorithms.countingSort(arr7);
+        System.out.println(Arrays.toString(arr7));
+
 
 
     }
