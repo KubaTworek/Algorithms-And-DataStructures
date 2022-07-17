5dc44d1135e91d77aee18d0b354895647c9730ac
KubaTworek <kubatvorek@gmail.com>
2022-07-17 04:28:36 +0200

    Data Strcutures
    Prepared classes for data structures and for new sort algorithm

--- src/algorithms/SortingAlgorithms.java
+++ src/algorithms/SortingAlgorithms.java
@@ -69,6 +69,10 @@ public class SortingAlgorithms {
 
     }
 
+    public static void bucketSort(int[] arr){
+
+    }
+
     private static void swap(int[] arr, int a, int b){
         int temp = arr[a];
         arr[a] = arr[b];
--- src/pl/pjatk/Main.java
+++ src/pl/pjatk/Main.java
@@ -16,6 +16,7 @@ public class Main {
         int[] arr6 = {3,35,-23,1,46,-90};
         int[] arr7 = {3,5,1,1,8,2,10,2};
         int[] arr8 = {2564,8136,3410,3156,9265,3850};
+        int[] arr9 = {3,35,-23,1,46,-90};
 
         SortingAlgorithms.bubbleSort(arr1);
         System.out.println(Arrays.toString(arr1));
